cda581a809613da07dcb23c1db689ddfec6c8699
swastia <44293051+swastia@users.noreply.github.com>
2021-01-15 10:26:49 +0530

    Myntra users API with eureka registry

---
+++ myntraApiUsers/myntraApiUsers/src/main/java/com/sarya/myntraApiUsers/MyntraApiUsersApplication.java
@@ -0,0 +1,13 @@
+package com.sarya.myntraApiUsers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyntraApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyntraApiUsersApplication.class, args);
+	}
+
+}
---
+++ myntraApiUsers/myntraApiUsers/src/test/java/com/sarya/myntraApiUsers/MyntraApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarya.myntraApiUsers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyntraApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
